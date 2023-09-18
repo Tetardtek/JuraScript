8ffa16927747306db77ea24f0b5105a4a8f2819e
Kevin Turnaco <kvnn64@gmail.com>
2023-09-18 09:30:29 +0200

    Changes

--- exercice5.js
+++ exercice5.js
@@ -1,15 +1,17 @@
 // Declare and define three variables using a numerical value
 
 // * If the three variables are equal, display "The three variables are the same" in the console
-
 // * If only two of the variables are equal, display "Two of the variables are the same" in the console
-
 // * If the variables are all different display "all variables are different" in the console
 
-let a = 2;
-let b = 2;
-let c = 2;
+const a = 2;
+const b = 2;
+const c = 2;
 
-if (a === b && b === c && c === a) {console.log('The three variables are the same')}
-else if (a === b || b === c || c === a) {console.log('Two of the variables are the same')}
-else {console.log('All variables are different')};
+if (a === b && b === c) {
+console.log("The three variables are the same");
+} else if (a === b || b === c || a === c) {
+console.log("Two of the variables are the same");
+} else {
+console.log("All variables are different");
+}

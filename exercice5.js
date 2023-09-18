82c0fd3c67729af26a85cac25868e8a230b19742
Kevin Turnaco <kvnn64@gmail.com>
2023-09-18 18:50:29 +0200

    Add all exercice and Js page to, now have to do them :D

---
+++ exercice5.js
@@ -0,0 +1,65 @@
+// Here are our Astro signs provided as a string.
+
+const aries = `♈`,
+taurus = `♉`,
+gemini = `♊`,
+cancer = `♋`,
+leo = `♌`,
+virgo = `♍`,
+libra = `♎`,
+scorpio = `♏`,
+sagittarius = `♐`,
+capricorn = `♑`,
+aquarius = `♒`,
+pisces = `♓︎`;
+// In Western Astrology there are 12 signs, organized by Earth Elements (Eart, Water, Air, Fire)
+
+// You are going to manipulate the following arrays along with this challenge:
+
+const fireSigns = [aries, leo];
+const earthSigns = [taurus, virgo, capricorn, sagittarius];
+const airSigns = [pisces, gemini, libra, aquarius];
+const waterSigns = [scorpio, pisces];
+/* 🏁 Add one final element to an array
+
+Sagittarius is missing from fire signs please add it at the
+
+END of the array and verify the result.
+
+// ✒️ Write your code here
+
+console.log(
+fireSigns[fireSigns.length - 1] === "♐"
+? "Good Answer ✅"
+: "Wrong Answer ❌"
+);
+/* 🏁 Remove the last element of an array
+Sagittarius should not be on earth Signs, please remove
+Sagittarius from the array, and verify the result.
+*/
+
+// ✒️ Write your code here
+
+// the line below is for testing, don't touch it :)
+
+console.log(earthSigns[earthSigns.length - 1] !== "♐" ? "Good Answer ✅" : "Wrong Answer ❌");
+/* 🏁 Remove one element at the begining of an array
+Pisces should not be on air Signs, please remove Pisces
+from the array, and verify the result.
+*/
+
+// ✒️ Write your code here
+
+// the line below is for testing, don't touch it :)
+
+console.log(airSigns[0] !== "♓︎" ? "Good Answer ✅" : "Wrong Answer ❌");
+/* 🏁 Add one element at the beginning of an array
+Cancer is missing from water signs please add it at the
+BEGINNING of the array and verify the result.
+*/
+
+// ✒️ Write your code here
+
+// the line below is for testing, don't touch it :)
+
+console.log(waterSigns[0] === "♋" ? "Good Answer ✅" : "Wrong Answer ❌");
